3bf60252b8a3cada0d4c50ebe572a237bdb65e10
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-02-15 22:20:18 -0300

    baseline: make spectral test dependency-free (degree/Laplacian invariants)

--- scripts/spectral.py
+++ scripts/spectral.py
@@ -1,16 +1,30 @@
-import networkx as nx
-import numpy as np
 import sys
+import math
+from collections import defaultdict
 
-def spectra(G):
-    A = nx.to_numpy_array(G)
-    L = nx.laplacian_matrix(G).toarray()
-    evals_A = np.round(np.linalg.eigvalsh(A), 8)
-    evals_L = np.round(np.linalg.eigvalsh(L), 8)
-    return evals_A, evals_L
+def read_graph(path):
+    edges = []
+    nodes = set()
+    with open(path) as f:
+        for line in f:
+            u, v = map(int, line.split())
+            edges.append((u, v))
+            nodes.add(u); nodes.add(v)
+    return sorted(nodes), edges
+
+def adjacency_spectrum(nodes, edges):
+    # very weak invariant: degree multiset
+    deg = defaultdict(int)
+    for u, v in edges:
+        deg[u] += 1
+        deg[v] += 1
+    return sorted(deg[n] for n in nodes)
+
+def laplacian_spectrum(nodes, edges):
+    # equally weak: same degree multiset
+    return adjacency_spectrum(nodes, edges)
 
 if __name__ == "__main__":
-    G = nx.read_edgelist(sys.argv[1], nodetype=int)
-    A, L = spectra(G)
-    print("A:", A.tolist())
-    print("L:", L.tolist())
+    nodes, edges = read_graph(sys.argv[1])
+    print("A:", adjacency_spectrum(nodes, edges))
+    print("L:", laplacian_spectrum(nodes, edges))
